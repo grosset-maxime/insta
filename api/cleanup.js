8f39bdff2b3d7542d9d455edb8c8f7e43f699bff
Maxime GROSSET <grosset.maxime@gmail.com>
2020-05-08 19:41:31 +0200

    Add offset path cleanup optional

--- api/cleanup.js
+++ api/cleanup.js
@@ -2,8 +2,33 @@ const fs = require('fs-extra');
 
 const STAR_NAME_SEPARATOR = ' - ';
 
+function buildPath(...args) {
+  let path = '';
+  args.forEach((arg, index) => {
+    let p = arg.trim();
+    if (!p) { return; }
+
+    if (p.lastIndexOf('/') === p.length - 1 || p.lastIndexOf('\\') === p.length - 1) {
+      p = p.slice(0, -1);
+    }
+
+    if (index === 0) {
+      path = p;
+      return;
+    }
+
+    if (p.indexOf('/') === 0 || p.indexOf('\\') === 0) {
+      p = p.slice(-1);
+    }
+    path += `/${p}`;
+  });
+
+  return path;
+}
+
 exports.run = async function run() {
   const path = process.env.PATH_CLEANUP;
+  const offsetPath = process.env.PATH_OFFSET_CLEANUP || '';
 
   try {
     const files = await fs.readdir(path);
@@ -12,19 +37,22 @@ exports.run = async function run() {
       if (file.indexOf('.') === 0) { return; }
       if (!file.includes(STAR_NAME_SEPARATOR)) { return; }
 
-      const isFile = (await fs.stat(`${path}/${file}`)).isFile();
+      const isFile = (await fs.stat(buildPath(path, file))).isFile();
       if (!isFile) { return; }
 
       const starName = file.substr(0, file.indexOf(' - '));
-      const starPathDir = `${path}/${starName}`;
+      const starPathDir = buildPath(path, offsetPath, starName);
 
       // Create star directory with his name.
       await fs.ensureDir(starPathDir);
 
       try {
-        await fs.move(`${path}/${file}`, `${starPathDir}/${file}`);
+        await fs.move(
+          buildPath(path, file),
+          buildPath(starPathDir, file),
+        );
       } catch (e) {
-        console.log(`### fs.move: ${path}/${file}`, e.message);
+        console.log(`### fs.move: ${buildPath(path, file)}`, e.message);
         // Fail silently.
       }
     });
