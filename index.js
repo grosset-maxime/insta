acf1321e3e6e3fd105d38cbbf275bc2d0686f283
Grosset Maxime <grosset.maxime@gmail.com>
2020-05-08 14:54:01 +0200

    Fix eol eslint

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const cleanup = require('./api/cleanup');
+
 require('dotenv').config();
 
 const app = express();
@@ -10,49 +12,49 @@ app.set('view engine', 'hbs');
 
 // Sets handlebars configurations
 app.engine('hbs', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs',
-    // defaultLayout: 'planB',
+  layoutsDir: `${__dirname}/views/layouts`,
+  extname: 'hbs',
+  // defaultLayout: 'planB',
 }));
 
 app.use(express.static('public'));
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-    // Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {
-        layout: 'index',
-        helpers: {}
-    });
-    // res.render('main');
+  // Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render('main', {
+    layout: 'index',
+    helpers: {},
+  });
+  // res.render('main');
 });
 
 app.get('/api/cleanup', async (req, res) => {
-    // console.log('### req:', req);
+  // console.log('### req:', req);
 
-    if (process.env.PATH_CLEANUP == null) {
-        throw new Error('Missing process.env.PATH_CLEANUP.');
-    }
+  if (process.env.PATH_CLEANUP == null) {
+    throw new Error('Missing process.env.PATH_CLEANUP.');
+  }
 
-    let success;
-    const cleanup = require('./api/cleanup');
+  let success;
 
-    try {
-        await cleanup.run();
-        // await cleanup.test();
-        success = true;
-    } catch (e) {
-        console.error(e);
-        success = false;
-    }
 
-    const resp = {
-        success,
-        PATH_CLEANUP: process.env.PATH_CLEANUP
-    }
+  try {
+    await cleanup.run();
+    // await cleanup.test();
+    success = true;
+  } catch (e) {
+    console.error(e);
+    success = false;
+  }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(resp));
+  const resp = {
+    success,
+    PATH_CLEANUP: process.env.PATH_CLEANUP,
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(resp));
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
