ba4b29b710fdb227c560400d0e37226b5793d370
Grosset Maxime <grosset.maxime@gmail.com>
2020-05-25 10:20:21 +0200

    Highlight of item on select

--- shortkeys/of/Of.js
+++ shortkeys/of/Of.js
@@ -2,14 +2,13 @@
 /* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.0.1';
+  const version = '1.0.2';
   const name = 'Of';
   const shortcut = 'ctrl+up';
   /* eslint-enable no-unused-vars */
 
   window._of = window._of || [];
 
-  // const STAR_NAME_EL_CLASS = '.b-profile__names .g-user-username';
   const VIDEO_CTN_CLASS = '.vjs-tech';
   const IMG_CTN_CLASS = '.pswp__item';
 
@@ -19,17 +18,28 @@
     }
   }
 
+  function highlight(el) {
+    el.style.transition = '.4s';
+    el.style.transform = 'scale(.98, .98)';
+    el.style['box-shadow'] = '0px 0 35px red';
+  }
+
+  // const refRect = document.querySelectorAll(POPUP_CTN_CLASS)[0].getBoundingClientRect();
+
   const imgs = document.querySelectorAll(`${IMG_CTN_CLASS} img`);
   (imgs || []).forEach((i) => {
     // if (i.classList.contains(UNWANTED_EL_CLASSES)) { return; }
-    push(i.src);
-    // highlight(i);
+    const rect = i.getBoundingClientRect();
+    if (rect.x >= 0 && rect.x < document.body.clientWidth) {
+      push(i.src);
+      highlight(i);
+    }
   });
 
   const vids = document.querySelectorAll(`${VIDEO_CTN_CLASS} source`);
   (vids || []).forEach((v) => {
     // if (v.classList.contains(UNWANTED_EL_CLASSES)) { return; }
     push(v.src);
-    // highlight(v);
+    highlight(v.parentElement);
   });
 })();
