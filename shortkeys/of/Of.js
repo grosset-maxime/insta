8a798807f16d112ec5a5fcc54d6b5fdc9319e0fc
Grosset Maxime <grosset.maxime@gmail.com>
2020-05-25 11:33:40 +0200

    Get items from messages

--- shortkeys/of/Of.js
+++ shortkeys/of/Of.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.0.2';
+  const version = '1.0.3';
   const name = 'Of';
   const shortcut = 'ctrl+up';
   /* eslint-enable no-unused-vars */
@@ -10,6 +10,7 @@
   window._of = window._of || [];
 
   const VIDEO_CTN_CLASS = '.vjs-tech';
+  const GIF_CTN_CLASS = '.post_gif_media';
   const IMG_CTN_CLASS = '.pswp__item';
 
   function push(src) {
@@ -24,22 +25,49 @@
     el.style['box-shadow'] = '0px 0 35px red';
   }
 
-  // const refRect = document.querySelectorAll(POPUP_CTN_CLASS)[0].getBoundingClientRect();
+  function isInScreen(el) {
+    let inScreen = true;
+    const rect = el.getBoundingClientRect();
+    const rectYMid = rect.y + rect.height / 2;
+
+    if (rectYMid < 0 || rectYMid > window.innerHeight) { inScreen = false; }
+    if (!inScreen || rect.x < 0 || rect.x + rect.width > window.innerWidth) { inScreen = false; }
+
+    return inScreen;
+  }
+
+  function iterateVids(vids, isGif = false) {
+    (vids || []).forEach((v) => {
+      // if (v.classList.contains(UNWANTED_EL_CLASSES)) { return; }
+
+      const label = v.getAttribute('label');
+      if (label) {
+        if (label !== 'original') { return; }
+        if (!isInScreen(v.parentElement)) { return; }
+      }
+
+      if (isGif) {
+        if (!isInScreen(v.parentElement)) { return; }
+      }
+
+      push(v.src);
+      highlight(v.parentElement);
+    });
+  }
 
   const imgs = document.querySelectorAll(`${IMG_CTN_CLASS} img`);
   (imgs || []).forEach((i) => {
     // if (i.classList.contains(UNWANTED_EL_CLASSES)) { return; }
     const rect = i.getBoundingClientRect();
-    if (rect.x >= 0 && rect.x < document.body.clientWidth) {
+    if (rect.x >= 0 && rect.x < window.innerWidth) {
       push(i.src);
       highlight(i);
     }
   });
 
   const vids = document.querySelectorAll(`${VIDEO_CTN_CLASS} source`);
-  (vids || []).forEach((v) => {
-    // if (v.classList.contains(UNWANTED_EL_CLASSES)) { return; }
-    push(v.src);
-    highlight(v.parentElement);
-  });
+  iterateVids(vids);
+
+  const gifs = document.querySelectorAll(`${GIF_CTN_CLASS} source`);
+  iterateVids(gifs, true);
 })();
