09cf3d12e91fd233d87dc5ab824e011b104e91f6
Grosset Maxime <grosset.maxime@gmail.com>
2020-05-27 17:41:53 +0200

    Store last viewed star Name

--- shortkeys/devArt/devArtBack.js
+++ shortkeys/devArt/devArtBack.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.0.1';
+  const version = '1.0.2';
   const name = 'DevArtBack';
   const shortcut = 'backspace';
   /* eslint-enable no-unused-vars */
@@ -8,7 +9,9 @@
   const BACK_TO_WATCH_BTN_CLASS = '._1JKQ8';
   const BACK_BTN_CLASS = '._3ngkd';
   const TABS_BTN_CLASS = '._2qipq';
-  const STAR_NAME_CLASS = '._1FO8S';
+  const STAR_NAME1_CLASS = '._1FO8S';
+  const STAR_NAME2_CLASS = '._2YW0c';
+  const STAR_NAME_IN_LIST = '._1n__p';
 
   let backBtn = document.querySelector(BACK_TO_WATCH_BTN_CLASS);
   if (backBtn) {
@@ -16,46 +19,58 @@
   } else {
     backBtn = document.querySelector(BACK_BTN_CLASS);
 
-    if (backBtn) {
-      let starName;
+    if (!backBtn) { return; }
 
-      const starNameEl = document.querySelector(STAR_NAME_CLASS);
-      if (starNameEl) {
-        starName = starNameEl.textContent.toLowerCase();
-      }
-      backBtn.click();
+    let starName;
 
-      const i = setInterval(() => {
-        const tabs = document.querySelectorAll(TABS_BTN_CLASS);
-
-        if (tabs && tabs.length) {
-          tabs[1].click();
-
-          if (starName) {
-            setTimeout(() => {
-              function scrollToBackStar() {
-                const stars = document.querySelectorAll('._1n__p');
-                const starEl = [].find.call(stars || [], (el) => starName === el.textContent.toLowerCase());
-
-                if (!starEl) { return false; }
-
-                starEl.parentElement.parentElement.parentElement.parentElement.parentElement.scrollIntoView();
-                document.documentElement.scrollTop -= 250;
-                return starEl;
-              }
-
-              if (!scrollToBackStar()) {
-                document.documentElement.scrollTop = document.documentElement.scrollHeight;
-                const ii = setInterval(() => {
-                  if (scrollToBackStar()) { clearInterval(ii); }
-                }, 50);
-              }
-            }, 50);
-          }
-
-          clearInterval(i);
-        }
-      }, 50);
+    let starNameEl = document.querySelector(STAR_NAME1_CLASS);
+    if (!starNameEl) {
+      starNameEl = document.querySelector(STAR_NAME2_CLASS);
     }
+    if (starNameEl) {
+      starName = starNameEl.textContent.toLowerCase();
+      window._lastViewedStarName = starName;
+    }
+
+    backBtn.click();
+
+    const i = setInterval(() => {
+      const tabs = document.querySelectorAll(TABS_BTN_CLASS);
+
+      if (tabs && tabs.length) {
+        tabs[1].click();
+
+        if (starName) {
+          setTimeout(() => {
+            function scrollToBackStar() {
+              const stars = document.querySelectorAll(STAR_NAME_IN_LIST);
+              const starEl = [].find.call(
+                stars || [], (el) => starName === el.textContent.toLowerCase(),
+              );
+
+              if (!starEl) { return false; }
+
+              starEl
+                .parentElement
+                .parentElement
+                .parentElement
+                .parentElement
+                .parentElement.scrollIntoView();
+              document.documentElement.scrollTop -= 250;
+              return starEl;
+            }
+
+            if (!scrollToBackStar()) {
+              document.documentElement.scrollTop = document.documentElement.scrollHeight;
+              const ii = setInterval(() => {
+                if (scrollToBackStar()) { clearInterval(ii); }
+              }, 50);
+            }
+          }, 50);
+        }
+
+        clearInterval(i);
+      }
+    }, 50);
   }
 })();
