8459cdafdb79662e45ff7ba786481f818ab8a4d5
Grosset Maxime <grosset.maxime@gmail.com>
2020-05-27 17:40:39 +0200

    Add DevArt remove shortkeys

---
+++ shortkeys/devArt/devArtRemove.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-underscore-dangle */
+(() => {
+  /* eslint-disable no-unused-vars */
+  const version = '1.0.0';
+  const name = 'DevArtRemove';
+  const shortcut = 'del';
+  /* eslint-enable no-unused-vars */
+
+  const STAR_NAME_IN_LIST = '._1n__p';
+  const REMOVE_BTN_CLASS = '._9rMMY';
+
+  const starName = window._lastViewedStarName;
+
+  if (!starName) { return false; }
+
+  const stars = document.querySelectorAll(STAR_NAME_IN_LIST);
+  const starEl = [].find.call(stars || [], (el) => starName === el.textContent.toLowerCase());
+
+  if (!starEl) { return false; }
+
+  const removeBtn = starEl
+    .parentElement
+    .parentElement
+    .parentElement
+    .parentElement
+    .parentElement.querySelector(REMOVE_BTN_CLASS);
+  if (removeBtn) {
+    removeBtn.click();
+  }
+
+  return true;
+})();
