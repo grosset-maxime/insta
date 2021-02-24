d3c8ae91c05eeb35d3459e6c2058995d6ce3c1e0
Grosset Maxime <grosset.maxime@gmail.com>
2021-02-24 14:29:25 +0100

    Highlight in blue already pushed src

--- shortkeys/of/Of.js
+++ shortkeys/of/Of.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.0.3';
+  const version = '1.0.4';
   const name = 'Of';
   const shortcut = 'ctrl+up';
   /* eslint-enable no-unused-vars */
@@ -13,16 +13,21 @@
   const GIF_CTN_CLASS = '.post_gif_media';
   const IMG_CTN_CLASS = '.pswp__item';
 
+  function hasPushSrc(src) {
+    return window._of.find((a) => a === src);
+  }
+
   function push(src) {
-    if (!window._of.find((a) => a === src)) {
+    if (!hasPushSrc(src)) {
       window._of.push(src);
     }
   }
 
-  function highlight(el) {
+  function highlight(el, src) {
     el.style.transition = '.4s';
     el.style.transform = 'scale(.98, .98)';
-    el.style['box-shadow'] = '0px 0 35px red';
+    const shadowColor = hasPushSrc(src) ? 'aqua' : 'red';
+    el.style['box-shadow'] = `0 0 35px ${shadowColor}`;
   }
 
   function isInScreen(el) {
@@ -50,8 +55,8 @@
         if (!isInScreen(v.parentElement)) { return; }
       }
 
+      highlight(v.parentElement, v.src);
       push(v.src);
-      highlight(v.parentElement);
     });
   }
 
@@ -60,8 +65,8 @@
     // if (i.classList.contains(UNWANTED_EL_CLASSES)) { return; }
     const rect = i.getBoundingClientRect();
     if (rect.x >= 0 && rect.x < window.innerWidth) {
+      highlight(i, i.src);
       push(i.src);
-      highlight(i);
     }
   });
 
