f86eb80c75de788e87e13ec5f785c5cca484066d
Grosset Maxime <grosset.maxime@gmail.com>
2021-05-22 11:48:58 +0200

    Enh ofright and of left

--- shortkeys/of/OfLeft.js
+++ shortkeys/of/OfLeft.js
@@ -2,13 +2,19 @@
 /* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.0.0';
+  const version = '1.0.1';
   const name = 'OfLeft';
   const shortcut = 'left';
   /* eslint-enable no-unused-vars */
 
-  const nbRightEl = document.querySelector('.nbright');
-  if (nbRightEl) {
-    (nbRightEl.textContent = parseInt(nbRightEl.textContent, 10) - 1);
+  const urlPathname = window.location.pathname;
+  const isTabPhotos = urlPathname.lastIndexOf('/photos') > 0;
+  const isTabVideos = urlPathname.lastIndexOf('/videos') > 0;
+
+  if (isTabPhotos || isTabVideos) {
+    const nbRightEl = document.querySelector('.nbright');
+    if (nbRightEl) {
+      (nbRightEl.textContent = parseInt(nbRightEl.textContent, 10) - 1);
+    }
   }
 })();
--- shortkeys/of/OfRight.js
+++ shortkeys/of/OfRight.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-underscore-dangle */
 (() => {
   /* eslint-disable no-unused-vars */
-  const version = '1.1.0';
+  const version = '1.1.1';
   const name = 'OfRight';
   const shortcut = 'right';
   /* eslint-enable no-unused-vars */
@@ -20,10 +20,13 @@
 
   let nbPostEl = document.querySelector('.nbpost');
   let nbRightEl = document.querySelector('.nbright');
-  let nbPostLoadedEl = document.querySelector('.nbpostloaded');
-  let nbRightLoadedEl = document.querySelector('.nbrightloaded');
+  const urlPathname = window.location.pathname;
+  const isTabPhotos = urlPathname.lastIndexOf('/photos') > 0;
+  const isTabVideos = urlPathname.lastIndexOf('/videos') > 0;
 
   function runOnScroll() {
+    if (isTabPhotos || isTabVideos) { return; }
+
     const posts = document.querySelectorAll(POST_EL_CLASS);
 
     if (!window._postIdSet) {
@@ -31,7 +34,7 @@
     }
 
     posts.forEach((el) => { window._postIdSet.add(el.id); });
-    nbRightLoadedEl.textContent = window._postIdSet.size;
+    nbRightEl.textContent = window._postIdSet.size;
   }
 
   if (!nbPostEl) {
@@ -61,35 +64,16 @@
     nbRightEl = document.querySelector('.nbright');
   }
 
-  if (!nbPostLoadedEl) {
-    const totalNbPosts = (document.querySelector(NB_POST_EL_CLASS) || {}).textContent || '';
+  if (isTabPhotos || isTabVideos) {
+    if (window._countNbLoadedPostsOnScroll) {
+      window.removeEventListener('scroll', window._countNbLoadedPostsOnScroll);
+      delete window._countNbLoadedPostsOnScroll;
+      nbRightEl.textContent = '1';
+    }
 
-    nbPostLoadedEl = document.createElement('span');
-    nbPostLoadedEl.classList.add('nbpostloaded');
-
-    nbRightLoadedEl = document.createElement('span');
-    nbRightLoadedEl.classList.add('nbrightloaded');
-
-    nbPostLoadedEl.style.position = 'fixed';
-    nbPostLoadedEl.style.left = '10px';
-    nbPostLoadedEl.style.top = '40px';
-    nbPostLoadedEl.style.color = 'black';
-    nbPostLoadedEl.style['font-size'] = '20px';
-    nbPostLoadedEl.style['z-index'] = '2000';
-    nbPostLoadedEl.style.background = '#ffffff30';
-    nbPostLoadedEl.style.padding = '3px 6px';
-    nbPostLoadedEl.style['border-radius'] = '6px';
-
-    nbPostLoadedEl.appendChild(nbRightLoadedEl);
-    nbPostLoadedEl.innerHTML = `${nbPostLoadedEl.innerHTML} / ${totalNbPosts}`;
-    document.body.appendChild(nbPostLoadedEl);
-
-    nbPostLoadedEl = document.querySelector('.nbpostloaded');
-    nbRightLoadedEl = document.querySelector('.nbrightloaded');
-
-    const debounced = debounce(() => runOnScroll());
-    window.addEventListener('scroll', debounced, { passive: true });
+    nbRightEl.textContent = parseInt(nbRightEl.textContent || 1, 10) + 1;
+  } else if (!window._countNbLoadedPostsOnScroll) {
+    window._countNbLoadedPostsOnScroll = debounce(() => runOnScroll());
+    window.addEventListener('scroll', window._countNbLoadedPostsOnScroll, { passive: true });
   }
-
-  nbRightEl.textContent = parseInt(nbRightEl.textContent || 1, 10) + 1;
 })();
